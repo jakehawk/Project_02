167b96a10697e588e0411dd32a5316e3be2b7ff5
Jake Hawkinson <jakehawk1095@gmail.com>
2017-02-27 11:37:37 -0800

    fixed email not connecting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ private
   def user_params
     # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
     # that can be submitted by a form to the user model #=> require(:user)
-    params.require(:user).permit(:name, :username, :password, :password_confirmation)
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
   end
 
 end
