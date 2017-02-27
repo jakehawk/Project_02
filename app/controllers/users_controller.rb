9e235ddb814d797dddeadba0d0026b2ab7e1e58c
Jake Hawkinson <jakehawk1095@gmail.com>
2017-02-26 22:49:23 -0800

    added basic login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
+    @user.username.downcase!
+
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
+    # that can be submitted by a form to the user model #=> require(:user)
+    params.require(:user).permit(:name, :username, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'users#welcome'
+
+  get 'users/new' => 'users#new', as: :new_user
+  get 'users/welcome' => 'users#welcome'
+  post 'users' => 'users#create'
+
+  get '/login'     => 'sessions#new'
+  post '/login'    => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
 end
